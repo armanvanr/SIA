105ed0e94225cf0b5d06c27eb0e2c3b9bae2ac6b
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2023-06-06 08:32:29 +0700

    update a course OK

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def get_courses():
 def get_course(code):
     course = Mata_Kuliah.query.filter_by(kode_mk=code).first()
     if not course:
-        return {"message": "Course not found"},404
+        return {"message": "Course not found"}, 404
     res = {"kode": course.kode_mk, "mata_kuliah": course.nama_mk, "sks": course.sks}
     return jsonify(res)
 
@@ -70,7 +70,8 @@ def add_course():
     if any([not "kode" in data, not "nama" in data, not "sks" in data]):
         return {"error": "Bad Request: Missing field(s)"}, 400
 
-    if Mata_Kuliah.query.filter_by(kode_mk=data["kode"]).first():
+    course = Mata_Kuliah.query.filter_by(kode_mk=data["kode"]).first()
+    if course:
         return {"error": "Course already exists"}, 400
 
     new_course = Mata_Kuliah(
@@ -79,6 +80,20 @@ def add_course():
     db.session.add(new_course)
     db.session.commit()
     return {"message": "Course added"}, 201
+
+
+# update a course
+@app.put("/course")
+def update_course():
+    data = request.get_json()
+    course = Mata_Kuliah.query.get(data["kode"])
+    if not course:
+        return {"message": "Course not found"}, 404
+    course.kode_mk = data.get("kode", course.kode_mk)
+    course.nama_mk = data.get("nama", course.nama_mk)
+    course.sks = data.get("sks", course.sks)
+    db.session.commit()
+    return {"message": "Course updated"}
 
 
 # delete a course
